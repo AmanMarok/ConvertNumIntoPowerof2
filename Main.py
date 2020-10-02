2a29bf42edbd7c1167bc6d8ab3297da705133d87
AmanMarok <39772169+AmanMarok@users.noreply.github.com>
2020-10-02 14:48:17 +0530

    added more inteligence

--- Main.py
+++ Main.py
@@ -7,14 +7,19 @@ def main():
             b += 1
             if a == 1:
                 break
-        print(f"{num} = 2^{b}\n")
+        if b == num:
+              print("!!!not possible, enter another number!!!\n")
+              quit
+        else:
+          print(f"{num} = 2^{b}\n")
 
     num = int(input("Enter Even number: "))
 
     if (num % 2) == 0:
         power_of_2()
-		else:
-        print("!! Only Even number is accepted !!\n")
-        exit
+    else:
+      print("Only even number is accepted\n")
+      exit  
+
 while True or False:
-    main()
+  main()
